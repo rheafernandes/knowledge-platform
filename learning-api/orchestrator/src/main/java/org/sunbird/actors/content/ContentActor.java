e7204e3cc5eec9f6fb112005bd448fcfb380b38e
Mahesh Kumar Gangula <mahesh@ilimi.in>
2019-10-14 15:12:39 +0530

    Issue #000 fix: compile fix.

--- learning-api/orchestrator/src/main/java/org/sunbird/actors/content/ContentActor.java
+++ learning-api/orchestrator/src/main/java/org/sunbird/actors/content/ContentActor.java
@@ -1,23 +1,23 @@
 package org.sunbird.actors.content;
 
 import akka.dispatch.Futures;
+import akka.dispatch.Mapper;
 import org.sunbird.actor.core.BaseActor;
 import org.sunbird.common.Platform;
 import org.sunbird.common.dto.Request;
 import org.sunbird.common.dto.Response;
 import org.sunbird.common.dto.ResponseHandler;
+import org.sunbird.graph.dac.model.Node;
 import org.sunbird.graph.nodes.DataNode;
 import scala.concurrent.Future;
 
 
 public class ContentActor extends BaseActor {
 
-    public static String objectType = "Content";
-    public static String version = "1.0";
-
     private static final String CONTENT_KEYSPACE_NAME = Platform.config.hasPath("content.keyspace.name") ? Platform.config.getString("content.keyspace.name") : "content_store";
     private static final String CONTENT_TABLE_NAME = Platform.config.hasPath("content.keyspace.table") ? Platform.config.getString("content.keyspace.table") : "content_data";
-
+    public static String objectType = "Content";
+    public static String version = "1.0";
 
     public Future<Response> onReceive(Request request) {
         String operation = request.getOperation();
@@ -41,13 +41,16 @@ public class ContentActor extends BaseActor {
         createRequest.getContext().put("table",CONTENT_TABLE_NAME);
         createRequest.getContext().put("graph_id", "domain");
         createRequest.getContext().put("version", "1.0");
-        Future<Response> result = DataNode.create(request, getContext().dispatcher()).map(node -> {
-                Response response = ResponseHandler.OK();
-                response.put("node_id", node.getIdentifier());
-                response.put("versionKey", node.getMetadata().get("versionKey"));
-                return response;
-        }, getContext().dispatcher());
-        return result;
+        return DataNode.create(createRequest, getContext().dispatcher())
+                .map(new Mapper<Node, Response>() {
+                    @Override
+                    public Response apply(Node node) {
+                        Response response = ResponseHandler.OK();
+                        response.put("node_id", node.getIdentifier());
+                        response.put("versionKey", node.getMetadata().get("versionKey"));
+                        return response;
+                    }
+                }, getContext().dispatcher());
     }
 
     private void update(Request request) {
