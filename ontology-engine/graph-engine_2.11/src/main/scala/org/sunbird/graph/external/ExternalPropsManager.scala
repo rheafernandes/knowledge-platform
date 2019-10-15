684e6f153eddb74ce618d9fa72a42bbd76e8729b
mrudulamayadeo <mrudula.m@mantralabs.com>
2019-10-15 16:01:00 +0530

    Returning Future[Response] from external store

--- ontology-engine/graph-engine_2.11/src/main/scala/org/sunbird/graph/external/ExternalPropsManager.scala
+++ ontology-engine/graph-engine_2.11/src/main/scala/org/sunbird/graph/external/ExternalPropsManager.scala
@@ -1,18 +1,19 @@
+
 package org.sunbird.graph.external
 
 import org.sunbird.common.dto.{Request, Response}
 import org.sunbird.graph.external.store.ExternalStoreFactory
+import org.sunbird.schema.SchemaValidatorFactory
 
 import scala.concurrent.{ExecutionContext, Future}
 
 object ExternalPropsManager {
-
     def saveProps(request: Request)(implicit ec: ExecutionContext): Future[Response] = {
-        val keySpace = request.getContext.get("keyspace").asInstanceOf[String]
-        val table = request.getContext.get("table").asInstanceOf[String]
-        val store = ExternalStoreFactory.getExternalStore(keySpace, table)
-        val response : Future[Response] = store.insert(request.getRequest)
-        response
+        val objectType: String = request.getObjectType
+        val version: String = request.getContext.get("version").asInstanceOf[String]
+        val store = ExternalStoreFactory.getExternalStore(SchemaValidatorFactory.getExternalStoreName(objectType, version))
+        store.insert(request.getRequest)
+        Future(new Response)
     }
 
 }
